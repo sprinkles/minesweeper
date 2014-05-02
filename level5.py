e69715a2a9f507b0fb53e774617853d32e9abf33
sprinkles <elizabeth.m.stanley@gmail.com>
2014-05-02 14:47:00 -0500

    Rewriting my reminder note as a little pseudocode

--- level5.py
+++ level5.py
@@ -1,4 +1,7 @@
 # Level 5
-# Define a "move" function that, when given a mine board and a status board, immediately calls the end-of-game checking function. If it returns 0, then the this function should return 2. Otherwise, this function prompts for a location and then uncovers that square using the function from Level 3. If the uncovering function returns a 0, this function should display the board using the procedure from Level 1 and return a 0. If the uncovering function returns a 1, then the pyrotechnic display procedure from Level 2 should be called and a value of 1 returned.
+# move(sboard,mboard):
+#   endOfGame==0 ? return 2 : pass
+#   loc=input
+#   showCell(loc) ? goBoom() return 1 : displayBoard return 0
 
 
